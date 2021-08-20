5748cba6542b5da70d14c66e939e2c6e0a1e7c7d
Dawid Myszczynski <myszczym@gmail.com>
2021-08-20 22:21:36 +0200

    Add some populate method

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -4,7 +4,7 @@ const Employee = require('../models/employees.model');
 
 router.get('/employees', async (req, res) => {
   try {
-    res.json(await Employee.find());
+    res.json(await Employee.find().populate('department'));
   }
   catch(err) {
     res.status(500).json({ message: err });
@@ -16,7 +16,7 @@ router.get('/employees/random', async (req, res) => {
   try {
     const count = await Employee.countDocuments();
     const rand = Math.floor(Math.random() * count);
-    const emp = await Employee.findOne().skip(rand);
+    const emp = await Employee.findOne().skip(rand).populate('department');
     if(!emp) res.status(404).json({ message: 'Not found' });
     else res.json(emp);
   }
@@ -29,7 +29,7 @@ router.get('/employees/random', async (req, res) => {
 router.get('/employees/:id', async (req, res) => {
 
   try {
-    const emp = await Employee.findById(req.params.id);
+    const emp = await Employee.findById(req.params.id).populate('department');
     if(!emp) res.status(404).json({ message: 'Not found' });
     else res.json(emp);
   }
@@ -77,7 +77,7 @@ router.put('/employees/:id', async (req, res) => {
 router.delete('/employees/:id', async (req, res) => {
 
   try {
-    const emp = await Employee.findById(req.params.id)
+    const emp = await Employee.findById(req.params.id).populate('department');
     if(emp) {
       await Employee.deleteOne({ _id: req.params.id });
       res.json(emp);
