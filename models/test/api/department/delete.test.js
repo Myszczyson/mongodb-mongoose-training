8fbc9576906079ccd6a13524215242c6c084b002
Dawid Myszczynski <myszczym@gmail.com>
2021-08-25 16:29:10 +0200

    Fix delet.test.js

--- models/test/api/department/delete.test.js
+++ models/test/api/department/delete.test.js
@@ -9,22 +9,15 @@ const expect = chai.expect;
 const request = chai.request;
 
 describe('DELETE /api/departments', () => {
-
-  it('/:id should delete chosen document and return success', () => {
     before(async () => {
         const testDepOne = new Department({ _id: '5d9f1140f10a81216cfd4408', name: 'Department #1' });
         await testDepOne.save();
     });
-    
+
     it('/:id should delete chosen document and return success', async () => {
         const res = await request(server).delete('/api/departments/5d9f1140f10a81216cfd4408');
+        const removedDepartment = await Department.findById('5d9f1140f10a81216cfd4408')
         expect(res.status).to.be.equal(200);
-        expect(res.body).to.be.null;
-      });
-
-    after(async () => {
-        await Department.deleteMany();
+        expect(removedDepartment).to.be.null;
     });
-  });
-
 });
